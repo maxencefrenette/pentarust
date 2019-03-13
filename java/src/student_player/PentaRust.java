2fbe2dbf47901d81ce598e3664a4f9b0ad2261eb
Maxence Frenette <maxence.frenette@gmail.com>
2019-03-13 03:17:36 -0400

    Simplify FFI

--- java/src/student_player/PentaRust.java
+++ java/src/student_player/PentaRust.java
@@ -5,6 +5,5 @@ public class PentaRust {
         System.load(System.getProperty("user.dir") + "/data/libPentaRust.so");
     }
 
-    private native long chooseMoveFFI(long player1, long player2);
-    public long chooseMove(long player1, long player2) { return chooseMoveFFI(player1, player2); }
+    public native long chooseMove(long player1, long player2);
 }
