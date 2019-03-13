57bfff2b0b06805930779c36380dfc8c3a6bca4f
Maxence Frenette <maxence.frenette@gmail.com>
2019-03-13 13:23:22 -0400

    Add java code to communicate with rust

--- java/src/student_player/StudentPlayer.java
+++ java/src/student_player/StudentPlayer.java
@@ -3,6 +3,9 @@ package student_player;
 import boardgame.Move;
 import pentago_swap.PentagoPlayer;
 import pentago_swap.PentagoBoardState;
+import pentago_swap.PentagoBoardState.Piece;
+import pentago_swap.PentagoBoardState.Quadrant;
+import pentago_swap.PentagoMove;
 
 /** A player file submitted by a student. */
 public class StudentPlayer extends PentagoPlayer {
@@ -24,9 +27,31 @@ public class StudentPlayer extends PentagoPlayer {
      * make decisions.
      */
     public Move chooseMove(PentagoBoardState boardState) {
-        System.out.println(pentaRust.chooseMove(0, 0));
+    	long player1 = 0;
+    	long player2 = 0;
+    	
+    	for(int i = 0; i < 6; i++) {
+    		for(int j = 0; j < 6; j++) {
+        		if (boardState.getPieceAt(j, i) == Piece.WHITE) {
+        			player1 &= 1 << (6*j + i);
+        		} else if (boardState.getPieceAt(j, i) == Piece.BLACK) {
+        			player2 &= 1 << (6*j + i);
+        		}
+        	}
+    	}
+    	
+        long bitMove = pentaRust.chooseMove(player1, player2);
+        long mask = 0xFF;
+        
+        int x = (int) (bitMove & mask);
+        int y = (int) (bitMove >> 8 & mask);
+        Quadrant aSwap = Quadrant.values()[(int) (bitMove >> 16 & mask)];
+        Quadrant bSwap = Quadrant.values()[(int) (bitMove >> 24 & mask)];
+        int playerId = (int) (bitMove >> 32 & mask);
 
         // Return random move for now
-        return boardState.getRandomMove();
+        PentagoMove m = new PentagoMove(x, y, aSwap, bSwap, playerId);
+        System.out.println(m.toPrettyString());
+        return m;
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@ use jni::JNIEnv;
 #[no_mangle]
 #[allow(non_snake_case)]
 pub extern "C" fn Java_student_1player_PentaRust_chooseMove(env: JNIEnv, player1: u64, player2: u64) -> u64 {
-    42
+    0x0000000000010000
 }
 
 #[cfg(test)]
