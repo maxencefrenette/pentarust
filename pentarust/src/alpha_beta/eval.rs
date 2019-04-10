47c8f8b200516902f591ac29a7fe288ba8da0e39
Maxence Frenette <maxence.frenette@gmail.com>
2019-04-10 19:09:06 -0400

    Optimized eval function

--- pentarust/src/alpha_beta/eval.rs
+++ pentarust/src/alpha_beta/eval.rs
@@ -5,20 +5,10 @@ pub fn eval(board: Board) -> i32 {
     let mut eval = 0;
 
     const CENTER_BONUS: i32 = 100;
-    const CENTERS: [u64; 4] = [
-        0b000000_010000_000000_000000_000000_000000,
-        0b000000_000010_000000_000000_000000_000000,
-        0b000000_000000_000000_000000_010000_000000,
-        0b000000_000000_000000_000000_000000_000010,
-    ];
+    const CENTERS: u64 = 0b000000_010010_000000_000000_010010_000000;
 
-    for c in CENTERS.iter() {
-        if board.player1 & c != 0 {
-            eval += CENTER_BONUS;
-        } else if board.player2 & c != 0 {
-            eval -= CENTER_BONUS;
-        }
-    }
+    eval += (board.player1 & CENTERS).count_ones() as i32 * CENTER_BONUS;
+    eval -= (board.player2 & CENTERS).count_ones() as i32 * CENTER_BONUS;
 
     if board.turn() == Player::Player1 {
         eval
